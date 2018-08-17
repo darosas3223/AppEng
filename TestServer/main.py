cb66255b9e6d8967c5df7859cdde816135fe1de1
David Rosas <41972687+darosas3223@users.noreply.github.com>
2018-08-17 10:26:00 -0700

    Update main.py

--- TestServer/main.py
+++ TestServer/main.py
@@ -1,5 +1,6 @@
 from google.appengine.api import images
 from google.appengine.ext import ndb
+from google.appengine.ext import blobstore
 from models import Note
 import webapp2
 
@@ -28,6 +29,9 @@ class InfoHandler(webapp2.RequestHandler):
 
         if descr == None:
             print("There's nothing in here. ")
+            
+        if self.request.get('img'):
+        	photo = Photo.get_by_id(int(self.request.get('img')))
 
     def post(self):
         print("Done.")
@@ -42,13 +46,32 @@ class InfoHandler(webapp2.RequestHandler):
         template = jinja_env.get_template('./submit.html')
         self.response.out.write(template.render())
 
+		
+class ViewInfo(webapp2.RequestHandler):
+	def _render_template(self, template_name, context = None):
+		if context is None:
+			context = {}
+		
+	def get(self):
+		qry = Note.owner_query()
+		context['notes'] = qry.fetch
+		template_name = jinja_env.get_template('./view.html')
+		return template.render(context)
+		
+
 class Note(ndb.Model):
     img = ndb.BlobProperty()
     describe = ndb.StringProperty()
+    
+    @classmethod
+    def owner_query(cls):
+        return cls.query().order(
+            cls.date_created)
 
 
 app = webapp2.WSGIApplication([
     ('/', InfoHandler),
     ('/sub', MainHandler),
     ('/view', InfoHandler),
+    ('/viewinfo', ViewInfo),
 ], debug=True)
